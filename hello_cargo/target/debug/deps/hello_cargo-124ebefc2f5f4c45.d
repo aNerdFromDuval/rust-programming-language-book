5c80e86aabd326e2e8f77374d9e06dd9e8117ba9
Replit user <>
2023-08-27 11:04:05 +0000

    removing hello_cargo

--- hello_cargo/target/debug/deps/hello_cargo-124ebefc2f5f4c45.d
+++
@@ -1,5 +0,0 @@
-/home/runner/rust-programming-language-book/hello_cargo/target/debug/deps/hello_cargo-124ebefc2f5f4c45: src/main.rs
-
-/home/runner/rust-programming-language-book/hello_cargo/target/debug/deps/hello_cargo-124ebefc2f5f4c45.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/runner/rust-programming-language-book/hello_cargo/target/debug/hello_cargo: /home/runner/rust-programming-language-book/hello_cargo/src/main.rs
